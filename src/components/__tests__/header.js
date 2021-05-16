8f3125d4fc26cd4ac0dd13d6a58d30a3c8f3a86c
Brandon Tom <magicink@gmail.com>
2021-05-16 05:27:08 -0700

    Added styled components

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -1,13 +1,20 @@
+import { render, screen } from '@testing-library/react'
 import React from "react"
 import renderer from "react-test-renderer"
 
 import Header from "../header"
 
+jest.mock('prop-types')
+
 describe("Header", () => {
-  it("renders correctly", () => {
+  it("matches snapshot", () => {
     const tree = renderer
       .create(<Header siteTitle="Default Starter" />)
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+  it('renders', () => {
+    render(<Header siteTitle={'test'} />)
+    expect(screen.getByTestId('site-header')).toBeInTheDocument()
   })
 })
